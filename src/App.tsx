9d1bc2e5cbc6eee06b79745a5c5881248c7c2c8e
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-03-18 19:22:24 -0600

    Expressions in JSX

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,21 @@ import './App.css';
 
 function App() {
 
+  const subtitulo = "Este es un subtitulo";
+
+  const duplicar = (valor: number) => valor * 2;
+
+  const imagenURL = "https://images.seeklogo.com/logo-png/14/1/tool-logo-png_seeklogo-140866.png";
+
   return(
     <div>
       <h1>Hola Mundo</h1>
-      <h4>Este es un subtitulo</h4>
+      <h4>{ subtitulo }</h4>
+      <h3>El doble de 3 es {duplicar(3)}</h3>
+      <img src={imagenURL} alt="logoTool" />
+      <div>
+        <input type="checkbox" checked={true}/>Este es un checkbox
+      </div>
     </div>
   );
 }
