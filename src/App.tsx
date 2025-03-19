3760aa026d57e733621b872149f81fe4423bb7d0
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-03-18 21:48:13 -0600

    Showing content by Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import MostrarTexto from './MostrarTexto';
+import ProyectarContenido from './ProyectarContenido';
+import ProyectarContenido2 from './ProyectarContenido2';
+import EjemploReloj from './EjemploReloj';
 
 function App() {
 
@@ -12,10 +15,32 @@ function App() {
     setTexto(e.currentTarget.value);
   }
 
+  const parteIntermedia = <EjemploReloj></EjemploReloj>
+
+  const estilo = {
+    background: 'red',
+    width: '50px',
+    height: '50px',
+    marginLeft: '1rem'
+  }
+  const parteInferior = <div style={estilo}></div>
+
   return(
     <div>
       <h1>Hola Mundo</h1>
+
+      <ProyectarContenido>
+        <>
+          <span>Componente para proyectar contenido</span>
+        </>
+      </ProyectarContenido>
   
+
+    <ProyectarContenido2
+      parteSuperior={<h3> Esta es la parte de arriba.</h3>}
+      parteIntermedia={parteIntermedia}
+      parteInferior={parteInferior}
+    ></ProyectarContenido2>
       <input type="text" 
         onKeyUp={(e) => manejarKeyUp(e) }
       />
