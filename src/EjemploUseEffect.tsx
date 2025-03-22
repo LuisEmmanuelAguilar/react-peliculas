2370b2d2d72967333350c3718c9794c8be1e54b3
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-03-22 13:45:16 -0600

    useEffect examples

--- src/EjemploUseEffect.tsx
+++ src/EjemploUseEffect.tsx
@@ -1,21 +1,43 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function EjemploUseEffect() {
     
+    const [clicks, setClicks] = useState(0);
+    const [fecha, setFecha] = useState(new Date())
+
     useEffect(() => {
-        console.log('componente ha cargado');
+        console.log('useEffect del click');
+        document.title = `${clicks} veces`
 
         return () => {
             // se ejecuta al destruir el componente
             console.log('el componente se va a destruir')
         }
+    }, [clicks])
+
+    useEffect(() => {
+        console.log('useEffect del interval');
+        const intervalId = setInterval(() => {
+            setFecha(new Date())
+        }, 1000)
+
+        return () => clearInterval(intervalId);
     })
 
     useEffect(() => {
-        console.log('componente ha cargado 2');
-    })
+        console.log('Voy a ejecutarme una sola vez')
+    }, [])
 
     return (
-        <span>Ejemplo useEffect</span>
+        <>
+            <button onClick={() => setClicks(clicks + 1) }>
+                Me has clickeado {clicks}  veces
+            </button>
+
+            <div>
+                { fecha.toString() }
+            </div>
+        </>
+
     )
 }
