e5fe26e0ba4b25133ca03c7b1b794412c11c9a05
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-03-24 21:22:32 -0600

    Loading component

--- src/peliculas/ListadoPeliculas.tsx
+++ src/peliculas/ListadoPeliculas.tsx
@@ -1,11 +1,12 @@
 import PeliculaIndividual from "./PeliculaIndividual";
 import { pelicula } from "./peliculas.model";
 import css from './ListadoPeliculas.module.css'
+import Cargando from "../utils/Cargando";
 
 export default function ListadoPeliculas(props: listadoPeliculasProps){
 
     if(!props.peliculas){
-        return <>Cargando...</>
+        return <Cargando/>
     } else if (props.peliculas.length === 0){
         return <>No hay elementos para mostrar</>
     } else {
