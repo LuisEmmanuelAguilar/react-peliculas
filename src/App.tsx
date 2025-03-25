d484597d3fedb8673c78d3b3a4e42fc07a4856c7
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-03-24 20:41:08 -0600

    Showing an individual movie

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
+import PeliculaIndividual from './peliculas/PeliculaIndividual';
+import { pelicula } from './peliculas/peliculas.model';
 
 function App() {
 
+  const peliculaPrueba: pelicula = {
+    id: 1, 
+    titulo: 'Spiderman', 
+    poster: 'https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg'  
+  }
+
   return(
     <>
-    hola mundo
+      <PeliculaIndividual pelicula={peliculaPrueba}/>
     </>
   );
 }
