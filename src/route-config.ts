3e390bbcdd7458112491c27883ac30b18eccf730
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-03-25 13:37:45 -0600

    Creating Genrers pages

--- src/route-config.ts
+++ src/route-config.ts
@@ -1,8 +1,12 @@
+import CrearGenero from "./generos/CrearGenero";
+import EditarGenero from "./generos/EditarGeenero";
 import IndiceGeneros from "./generos/IndiceGeneros";
 import LandingPage from "./LandingPage";
 
 const rutas = [
     { path: "/generos", componente: IndiceGeneros, exact: true },
+    { path: "/generos/crear", componente: CrearGenero, exact: true },
+    { path: "/generos/editar", componente: EditarGenero, exact: true },
     { path: "/", componente: LandingPage, exact: true }];
 
 export default rutas;
