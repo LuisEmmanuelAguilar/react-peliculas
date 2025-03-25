6cf90874c7fa840f3ef80fc45e9cf0bffba8e04e
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-03-24 23:49:20 -0600

    Setup React-Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,29 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import { landingPageDTO } from './peliculas/peliculas.model';
-import ListadoPeliculas from './peliculas/ListadoPeliculas';
-import { useEffect, useState } from 'react';
-import Button from './utils/Button';
+import IndiceGeneros from './generos/IndiceGeneros';
+import LandingPage from './LandingPage';
 import Menu from './utils/Menu';
 
 function App() {
 
-  const [peliculas, setPeliculas] = useState<landingPageDTO>({})
-
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      setPeliculas({
-          enCartelera: [
-            {
-              id: 1, titulo: 'Spider-Man: Far from Home',
-              poster: 'https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg'
-            },
-            {
-              id: 2, titulo: 'Moana',
-              poster: 'https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_UX182_CR0,0,182,268_AL_.jpg'
-            }
-          ],
-          proximosEstrenos: [
-            {
-              id: 3, titulo: 'Soul',
-              poster: 'https://m.media-amazon.com/images/M/MV5BZGE1MDg5M2MtNTkyZS00MTY5LTg1YzUtZTlhZmM1Y2EwNmFmXkEyXkFqcGdeQXVyNjA3OTI0MDc@._V1_UX182_CR0,0,182,268_AL_.jpg'
-            }
-          ]
-        }
-      )
-    }, 1000);
-
-    return () => clearTimeout(timerId);
-  })
 
   return(
     <>
+      <BrowserRouter>
         <Menu></Menu>
         <div className='container'>
-        <h3>En Cartelera</h3>
-        <ListadoPeliculas peliculas={peliculas.enCartelera}/>
+          <Switch>
+            <Route exact path="/">
+              <LandingPage></LandingPage>
+            </Route>
 
-        <h3>Proximos Estrenos</h3>
-        <ListadoPeliculas peliculas={peliculas.proximosEstrenos}/>
-      </div>
+            <Route path="/generos">
+              <IndiceGeneros></IndiceGeneros>
+            </Route>
+
+          </Switch>        
+        </div>
+      </BrowserRouter>
     </>
   );
 }
