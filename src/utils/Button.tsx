b4089f147a920c50afe046fcbba4f84183e4335a
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-03-25 16:40:49 -0600

    Bulding up form for create genre

--- src/utils/Button.tsx
+++ src/utils/Button.tsx
@@ -3,13 +3,19 @@ import { ReactNode } from "react";
 export default function Button(props: buttonProps){
     return (
         <button 
-            type="button" 
+            type={props.type} 
             className="btn btn-primary"
-            onClick={props.onClick}>{props.children}</button>
+            onClick={props.onClick}
+            >{props.children}</button>
     )
 }
 
 interface buttonProps{
     children: ReactNode;
-    onClick(): void;
+    onClick?(): void;
+    type: "button" | "submit"
+}
+
+Button.defaultProps = {
+    type: "button"
 }
