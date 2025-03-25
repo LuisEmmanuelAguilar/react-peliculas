9ef9d85ee99c95d059db1ef616bff5b892d56113
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-03-25 14:06:06 -0600

    Creating Theathers routes and pages

--- src/route-config.ts
+++ src/route-config.ts
@@ -1,6 +1,9 @@
 import CrearActor from "./actores/CrearActor";
 import EditarActor from "./actores/EditarActor";
 import IndiceActores from "./actores/IndiceActores";
+import CrearCine from "./cines/CrearCine";
+import EditarCine from "./cines/EditarCine";
+import IndiceCines from "./cines/IndiceCines";
 import CrearGenero from "./generos/CrearGenero";
 import EditarGenero from "./generos/EditarGeenero";
 import IndiceGeneros from "./generos/IndiceGeneros";
@@ -13,6 +16,9 @@ const rutas = [
     { path: "/actores/", componente: IndiceActores, exact: true },
     { path: "/actores/crear", componente: CrearActor, exact: true },
     { path: "/actores/editar", componente: EditarActor, exact: true },
+    { path: "/cines", componente: IndiceCines, exact: true },
+    { path: "/cines/crear", componente: CrearCine, exact: true },
+    { path: "/cines/editar", componente: EditarCine, exact: true },
     { path: "/", componente: LandingPage, exact: true }];
 
 export default rutas;
