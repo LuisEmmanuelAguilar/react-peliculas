cab8a7b4dbbe6681ae1dd55f557568add46d7a11
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-03-25 13:43:55 -0600

    Creating Actors pages and routes

--- src/route-config.ts
+++ src/route-config.ts
@@ -1,3 +1,6 @@
+import CrearActor from "./actores/CrearActor";
+import EditarActor from "./actores/EditarActor";
+import IndiceActores from "./actores/IndiceActores";
 import CrearGenero from "./generos/CrearGenero";
 import EditarGenero from "./generos/EditarGeenero";
 import IndiceGeneros from "./generos/IndiceGeneros";
@@ -7,6 +10,9 @@ const rutas = [
     { path: "/generos", componente: IndiceGeneros, exact: true },
     { path: "/generos/crear", componente: CrearGenero, exact: true },
     { path: "/generos/editar", componente: EditarGenero, exact: true },
+    { path: "/actores/", componente: IndiceActores, exact: true },
+    { path: "/actores/crear", componente: CrearActor, exact: true },
+    { path: "/actores/editar", componente: EditarActor, exact: true },
     { path: "/", componente: LandingPage, exact: true }];
 
 export default rutas;
