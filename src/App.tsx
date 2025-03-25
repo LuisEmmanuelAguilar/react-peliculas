cbeff9900aa0057430d7114be50bf1f8bb20b009
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-03-24 21:18:31 -0600

    useEffect and useState for loading list of movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
 import './App.css';
-import PeliculaIndividual from './peliculas/PeliculaIndividual';
-import { pelicula } from './peliculas/peliculas.model';
+import { landingPageDTO } from './peliculas/peliculas.model';
 import ListadoPeliculas from './peliculas/ListadoPeliculas';
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  const peliculasEnCartelera: pelicula[] = [
-    {
-      id: 1, titulo: 'Spider-Man: Far from Home',
-      poster: 'https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg'
-    },
-    {
-      id: 2, titulo: 'Moana',
-      poster: 'https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_UX182_CR0,0,182,268_AL_.jpg'
-    }
-  ];
+  const [peliculas, setPeliculas] = useState<landingPageDTO>({})
 
-  const peliculasProximosEstrenos: pelicula[] = [
-    {
-      id: 3, titulo: 'Soul',
-      poster: 'https://m.media-amazon.com/images/M/MV5BZGE1MDg5M2MtNTkyZS00MTY5LTg1YzUtZTlhZmM1Y2EwNmFmXkEyXkFqcGdeQXVyNjA3OTI0MDc@._V1_UX182_CR0,0,182,268_AL_.jpg'
-    }
-  ];
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setPeliculas({
+          enCartelera: [
+            {
+              id: 1, titulo: 'Spider-Man: Far from Home',
+              poster: 'https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg'
+            },
+            {
+              id: 2, titulo: 'Moana',
+              poster: 'https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_UX182_CR0,0,182,268_AL_.jpg'
+            }
+          ],
+          proximosEstrenos: [
+            {
+              id: 3, titulo: 'Soul',
+              poster: 'https://m.media-amazon.com/images/M/MV5BZGE1MDg5M2MtNTkyZS00MTY5LTg1YzUtZTlhZmM1Y2EwNmFmXkEyXkFqcGdeQXVyNjA3OTI0MDc@._V1_UX182_CR0,0,182,268_AL_.jpg'
+            }
+          ]
+        }
+      )
+    }, 1000);
+
+    return () => clearTimeout(timerId);
+  })
 
   return(
     <>
       <h3>En Cartelera</h3>
-      <ListadoPeliculas peliculas={peliculasEnCartelera}/>
+      <ListadoPeliculas peliculas={peliculas.enCartelera}/>
 
       <h3>Proximos Estrenos</h3>
-      <ListadoPeliculas peliculas={peliculasProximosEstrenos}/>
+      <ListadoPeliculas peliculas={peliculas.proximosEstrenos}/>
     </>
   );
 }
--- src/peliculas/ListadoPeliculas.tsx
+++ src/peliculas/ListadoPeliculas.tsx
@@ -4,17 +4,23 @@ import css from './ListadoPeliculas.module.css'
 
 export default function ListadoPeliculas(props: listadoPeliculasProps){
 
-    return (
-        <div className={css.div}>
-            {props.peliculas.map(
-                pelicula => <PeliculaIndividual 
-                            pelicula={pelicula}
-                            key={pelicula.id}/>
-                )}
-        </div>
-    )
+    if(!props.peliculas){
+        return <>Cargando...</>
+    } else if (props.peliculas.length === 0){
+        return <>No hay elementos para mostrar</>
+    } else {
+        return (
+            <div className={css.div}>
+                {props.peliculas.map(
+                    pelicula => <PeliculaIndividual 
+                                pelicula={pelicula}
+                                key={pelicula.id}/>
+                    )}
+            </div>
+        )
+    }
 }
 
 interface listadoPeliculasProps {
-    peliculas: pelicula[]
+    peliculas?: pelicula[]
 }
